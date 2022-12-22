7f63624e86a9cbb3817ecc465ca6aa885733ea2d
longcoat <sherlockpkm@gmail.com>
2022-12-22 13:22:19 +0900

    fresh start

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -12,7 +12,7 @@ export default function BoardsDetailPage() {
   );
 }
 
-export const getServerSideProps = () => {
-  // 만약 서버사이드 랜더링을 한다면?? => out 폴더로 생성 불가
-  //                           => next.config.js에서 exportPathMap으로 현재 페이지 제외시키기
-};
+// export const getServerSideProps = () => {
+//   // 만약 서버사이드 랜더링을 한다면?? => out 폴더로 생성 불가
+//   //                           => next.config.js에서 exportPathMap으로 현재 페이지 제외시키기
+// };
