c2d2699106e828dd2706cc3648a1f097654c6de2
longcoat <sherlockpkm@gmail.com>
2022-12-22 10:56:03 +0900

    fresh start33

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
+  generateBuildId: () => "codecamp_deploy_10",
+
+  //   아래 주소들만 out폴더에 만들어줘!!(getServerSideProps가 있는 페이지는 제외)
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -6,8 +6,13 @@ export default function BoardsDetailPage() {
   return (
     <div>
       안녕하세요 동적페이지입니다.
-      <br /> 
+      <br />
       게시글아이디: {router.query.boardId}
     </div>
   );
 }
+
+export const getServerSideProps = () => {
+  // 만약 서버사이드 랜더링을 한다면?? => out 폴더로 생성 불가
+  //                           => next.config.js에서 exportPathMap으로 현재 페이지 제외시키기
+};
